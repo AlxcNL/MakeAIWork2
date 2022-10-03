a0047e58fce724c66200a5b7859474f5f6b3c19c
Jeroen Boogaard <J.A.Boogaard@hr.nl>
2022-10-03 16:50:53 +0200

    Alternative pip installation of Conda libraries

--- install/install_requirements.sh
+++ install/install_requirements.sh
@@ -3,58 +3,77 @@
 # # Activate virtual environment env
 # source env/bin/activate
 
+function install_without_conda {
+  # Upgrade pip
+  python3 -m pip install --upgrade pip
+
+  # Install setuptools
+  python3 -m pip install setuptools
+  python3 -m pip install virtualenv
+  python3 -m venv env 
+  env/Scripts/activate
+  python -m pip install --no-cache-dir -r install/pip/no_conda.txt
+
+}  
+
+function install_with_conda {
+  if (! command -v "conda" &> /dev/null ) then
+    echo "Try to install basic Python requirements without Miniconda\n"
+    install_without_conda
+  else
+    conda install --yes -c conda-forge \
+      beautifulsoup4 \
+      jupyter_core \
+      jupyterlab \
+      keras \
+      Keras-Preprocessing \
+      matplotlib-base \
+      Pillow \
+      pandas \
+      py-cpuinfo \
+      pyopengl \
+      pytables \
+      scikit-image \
+      scikit-learn \
+      scipy \
+      seaborn \
+      selenium \
+      statsmodels \
+      sympy \
+      tensorflow
+  fi
+
+}
+
 # Detect OS
 unameOut="$(uname -s)"
 os="${unameOut:0:7}"
 case "${os}" in
     Linux*)     
       machine="Linux"
+      install_with_conda
     ;;
     Darwin*)
       machine="Mac"
-      ;;
-    CYGWIN*)    machine="Cygwin";;
-    MINGW*)     machine="Git Bash";;
-    *)          machine="UNKNOWN:${os}"
+      install_with_conda
+    ;;
+    MINGW*)     
+      machine="Git Bash"
+      install_without_conda
+    ;;
+    *)          
+      machine="UNKNOWN:${os}"
 esac
-
-function install_with_conda {
-  if (! command -v "conda" &> /dev/null ) then
-    echo "Install Miniconda first\n" && exit 0
-  fi
-
-  conda install --yes -c conda-forge \
-    beautifulsoup4 \
-    jupyter_core \
-    jupyterlab \
-    keras \
-    Keras-Preprocessing \
-    matplotlib-base \
-    Pillow \
-    pandas \
-    py-cpuinfo \
-    pyopengl \
-    pytables \
-    scikit-image \
-    scikit-learn \
-    scipy \
-    seaborn \
-    selenium \
-    statsmodels \
-    sympy \
-    tensorflow
-}
 
 # Install all required libraries t
 function install_with_pip {
   # Upgrade pip
-  python3 -m pip install --upgrade pip
+  python -m pip install --upgrade pip
 
   # Install setuptools
-  python3 -m pip install setuptools
-  python3 -m pip install --no-cache-dir -r install/pip/requirements.txt
+  python -m pip install setuptools
+  python -m pip install --no-cache-dir -r install/pip/requirements.txt
 
 }
 
-# install_with_conda
 install_with_pip
