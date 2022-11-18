4b7cf9b0195a407cb5228b0edb4ec24940d09788
djgnatzy <djgnatzy@gmail.com>
2022-11-18 15:37:33 +0100

    Miss Applebum is almost there

--- projects/apple_disease_classification/notebooks/chatbot/chatbot_bonita_applebum.py
+++ projects/apple_disease_classification/notebooks/chatbot/chatbot_bonita_applebum.py
@@ -99,7 +99,7 @@ def bag_of_words(s, words):
 
 
 def chat():
-    print("Start talking with the bot (type quit to stop)!")
+    print("Start talking with Botnita Applebum (type quit to stop)!")
     while True:
         inp = input("You: ")
         if inp.lower() == "quit":
