769694522f5c47ae691aebd49dbd64914f345f3c
djgnatzy <djgnatzy@gmail.com>
2022-11-21 13:34:22 +0100

    chatbot_variables_in_json

--- projects/apple_disease_classification/notebooks/chatbot/chatbot_bonita_applebum.py
+++ projects/apple_disease_classification/notebooks/chatbot/chatbot_bonita_applebum.py
@@ -2,12 +2,14 @@ import nltk
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
 
-import numpy
 import tflearn
+import numpy
 import tensorflow
 import random
 import json
 import pickle
+
+# import aql_apples.py
 
 with open("intents_apples.json") as file:
     data = json.load(file)
@@ -97,12 +99,51 @@ def bag_of_words(s, words):
             
     return numpy.array(bag)
 
-# def replaceResponse():
+
+sampleBatch = 80
+
+appleScore = 75
+blotchApple = 1
+rotApple = 1
+scabApple = 3
+rejectedApple = 5
+
+healthyPercentage = 93.5
+blotchPercentage = 1.25
+rotPercentage = 1.25
+scabPercentage = 2.5
+rejectedPercentage = 5
+
+classOne = "Class 1"
+classTwo = "Class 2"
+classThree = "Class 3"
+classRej = "Rejected"
+
+def replaceResponse(response):
     
-#     "The batch contains {scabApple} apples with scabs, this is {scabPercentage}% of the batch."
+    response = response.replace("(sampleBatch)", f"{sampleBatch}")
+    response = response.replace("(appleScore)", f"{appleScore}")
+    response = response.replace("(blotchApple)", f"{blotchApple}")
+    response = response.replace("(scabApple)", f"{scabApple}")
+    response = response.replace("(rotApple)", f"{rotApple}")
+    response = response.replace("(rejectedApple)", f"{rejectedApple}")
+    
+    response = response.replace("(healthyPercentage)", f"{healthyPercentage}")
+    response = response.replace("(blotchPercentage)", f"{blotchPercentage}")
+    response = response.replace("(rotPercentage)", f"{rotPercentage}")
+    response = response.replace("(scabPercentage)", f"{scabPercentage}")
+    response = response.replace("(rejectedPercentage)", f"{rejectedPercentage}")
+
+    response = response.replace("(classOne)", f"{classOne}")          # all to one variable
+    response = response.replace("(classTwo)", f"{classTwo}")
+    response = response.replace("(classThree)", f"{classThree}")
+    response = response.replace("(classRej)", f"{classRej}")
+    
+    return response
 
 def chat():
-    print("Start talking with Botnita Applebum (type quit to stop)!")
+    
+    print("Start talking with Botnita Applebum.\n","Ask here anything about apples.\n\n","(type quit to stop)")
     while True:
         inp = input("You: ")
         if inp.lower() == "quit":
@@ -114,7 +155,10 @@ def chat():
 
         for tg in data["intents"]:
             if tg['tag'] == tag:
-                responses = tg['responses']
+                responses = [replaceResponse(response)
+                             for response in tg['responses']]
+                
+                # responses = tg['responses']
 
         print(random.choice(responses))
 
