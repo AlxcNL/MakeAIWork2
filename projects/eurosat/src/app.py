77632bf78949a912982f867c8f0041ca6d9568c7
AlxcNL <J.A.Boogaard@hr.nl>
2022-11-22 21:03:28 +0100

    Display image batch

--- projects/eurosat/src/app.py
+++ projects/eurosat/src/app.py
@@ -1,48 +1,56 @@
+#!/usr/bin/env python
+
 from flask import Flask, flash, request, redirect, url_for, render_template
 import urllib.request
 import os
 from werkzeug.utils import secure_filename
 import urllib.request
+
 app = Flask(__name__)
-  
-UPLOAD_FOLDER = 'static/uploads/'
- 
-app = Flask(__name__)
-app.secret_key = "cairocoders-ednalan"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-  
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
- 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-     
-@app.route('/')
-def upload_form():
-    return render_template('upload.html')
- 
-@app.route('/', methods=['POST'])
+
+
+def isSupportedFileType(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in set(
+        ["png", "jpg", "jpeg", "gif"]
+    )
+
+
+@app.route("/")
+def uploadForm():
+    return render_template("upload.html")
+
+
+@app.route("/", methods=["POST"])
 def upload_image():
-    if 'files[]' not in request.files:
-        flash('No file part')
+
+    if "files[]" not in request.files:
+        flash("No file part")
         return redirect(request.url)
-    files = request.files.getlist('files[]')
-    file_names = []
+
+    files = request.files.getlist("files[]")
+    fileNames = list()
+    imgLabels = list()
+
     for file in files:
-        if file and allowed_file(file.filename):
+        if file and isSupportedFileType(file.filename):
             filename = secure_filename(file.filename)
-            file_names.append(filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            fileNames.append(filename)
+            imgLabels.append("label")
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+
         else:
-            flash('Allowed image types are -> png, jpg, jpeg, gif')
+            flash("Allowed image types are -> png, jpg, jpeg, gif")
             return redirect(request.url)
- 
-    return render_template('upload.html', filenames=file_names)
- 
-@app.route('/display/<filename>')
+
+    return render_template("upload.html", filenames=fileNames, labels=imgLabels)
+
+
+@app.route("/display/<filename>")
 def display_image(filename):
-    #print('display_image filename: ' + filename)
-    return redirect(url_for('static', filename='uploads/' + filename), code=301)
-  
+    return redirect(url_for("static", filename="uploads/" + filename), code=301)
+
+
 if __name__ == "__main__":
-    app.run()
+    app.config["UPLOAD_FOLDER"] = "static/uploads/"
+    app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
+    app.run(host="127.0.0.1", port="5000", debug=True)
