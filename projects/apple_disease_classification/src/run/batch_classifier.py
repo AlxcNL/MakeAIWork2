3c704a518fa29171cdec7aab50907935a2b1ef4e
djgnatzy <djgnatzy@gmail.com>
2022-11-22 23:41:08 +0100

    Setup Build & Run

--- projects/apple_disease_classification/src/run/batch_classifier.py
+++ projects/apple_disease_classification/src/run/batch_classifier.py
@@ -2,19 +2,16 @@
 
 # DEFINE IMPORTS HERE
 
-
-
-
-
-
-
-
+def getLabel():
+    
 
 
 # IMPLEMENT RUNNABLE CODE INSIDE THIS MAIN 
 def main():
     pass
+    
+
 
 # DO NOT IMPLEMENT ANYTHING HERE
-if __name__ == main():
-    main()    
+if __name__ == "__main__":
+    main()   
--- projects/apple_disease_classification/src/run/batch_generator.py
+++ projects/apple_disease_classification/src/run/batch_generator.py
@@ -3,10 +3,7 @@
 # DEFINE IMPORTS HERE
 
 
-
-
-
-
+def getBatch():
 
 
 
@@ -14,7 +11,9 @@
 # IMPLEMENT RUNNABLE CODE INSIDE THIS MAIN 
 def main():
     pass
+    
+
 
 # DO NOT IMPLEMENT ANYTHING HERE
-if __name__ == main():
-    main()    
+if __name__ == "__main__":
+    main()   
